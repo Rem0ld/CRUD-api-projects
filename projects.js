617c68e98ccda314a4223b4999efd15d31dfc8b4
Rem0ld <p.lovergne@hotmail.fr>
2020-11-11 22:33:17 +0100

    add seed with all my projects

---
+++ projects.js
@@ -0,0 +1,122 @@
+module.exports = [
+  {
+    websiteUrl: "https://terrasurabogados.cl",
+    codeUrl: "",
+    imageUrl:
+      "https://cdn.glitch.com/a56d7f44-3f97-4b8e-8e48-62256ce3c8d1%2FScreenshot_2020-08-26%20TerraSur%20Abogados%20-%20Asesoria%20Legal%20Lago%20LLanquihue.jpg?v=1598450959311",
+    altImage: "Terrasur abogados Landing Page",
+  },
+  {
+    websiteUrl: "https://tribute-page-napol.glitch.me/",
+    codeUrl: "https://glitch.com/edit/#!/tribute-page-napol",
+    imageUrl:
+      "https://cdn.glitch.com/a56d7f44-3f97-4b8e-8e48-62256ce3c8d1%2Fnapoleon.jpg?v=1585843736025",
+    altImage: "Tribute Page about Napoléon Bonaparte",
+  },
+  {
+    websiteUrl: "https://form-page.glitch.me/",
+    codeUrl: "https://glitch.com/edit/#!/form-page",
+    imageUrl:
+      "https://cdn.glitch.com/a56d7f44-3f97-4b8e-8e48-62256ce3c8d1%2Fform.jpg?v=1585843730316",
+    altImage: "Form webpage",
+  },
+  {
+    websiteUrl: "https://landing-page-sc.glitch.me/",
+    codeUrl: "https://glitch.com/edit/#!/landing-page-sc",
+    imageUrl:
+      "https://cdn.glitch.com/a56d7f44-3f97-4b8e-8e48-62256ce3c8d1%2Fscootrix.png?v=1585843741728",
+    altImage: "Landing page Scootrix",
+  },
+  {
+    websiteUrl: "https://doc-tech-rust.glitch.me/",
+    codeUrl: "https://glitch.com/edit/#!/doc-tech-rust",
+    imageUrl:
+      "https://cdn.glitch.com/e431f8fe-6864-4d58-a472-36bc6659a290%2FScreenshot_2020-08-26%20Documentation%20about%20Rust.png?v=1598450058798",
+    altImage: "Technical Documentation about Rust",
+  },
+  {
+    websiteUrl: "https://codesandbox.io/s/electro-player-pl-5if05",
+    codeUrl: "",
+    imageUrl:
+      "https://cdn.glitch.com/a56d7f44-3f97-4b8e-8e48-62256ce3c8d1%2FScreenshot_2020-08-26%20React%20App.png?v=1598453981464",
+    altImage: "Electro Player",
+  },
+  {
+    websiteUrl: "https://stackblitz.com/edit/markdown-previewpl?file=App.js",
+    codeUrl: "",
+    imageUrl:
+      "https://cdn.glitch.com/a56d7f44-3f97-4b8e-8e48-62256ce3c8d1%2FScreenshot_2020-08-26%20https%20markdown-previewpl%20stackblitz%20io.png?v=1598453976690",
+    altImage: "Mardown Preview",
+  },
+  {
+    websiteUrl: "https://stackblitz.com/edit/quote-machinepl",
+    codeUrl: "",
+    imageUrl:
+      "https://cdn.glitch.com/a56d7f44-3f97-4b8e-8e48-62256ce3c8d1%2FScreenshot_2020-08-26%20https%20quote-machinepl%20stackblitz%20io.png?v=1598454084183",
+    altImage: "Drum Player",
+  },
+  {
+    websiteUrl: "https://ld-page-frontend-mentor.vercel.app/",
+    codeUrl: "https://github.com/Rem0ld/ld-page-frontend-mentor",
+    imageUrl:
+      "https://raw.githubusercontent.com/Rem0ld/ld-page-frontend-mentor/master/images/Screenshot_2020-11-06%20Frontend%20Mentor%20Social%20proof%20section.png",
+    altImage: "Landing Page Frontend Mentor",
+  },
+  {
+    websiteUrl: "https://four-card-feature-fd-mentor.vercel.app/",
+    codeUrl: "https://github.com/Rem0ld/four-card-feature-fd-mentor",
+    imageUrl:
+      "https://raw.githubusercontent.com/Rem0ld/four-card-feature-fd-mentor/master/images/Screenshot_2020-11-06%20Frontend%20Mentor%20Four%20card%20feature%20section.png",
+    altImage: "Four card Frontend Mentor",
+  },
+  {
+    websiteUrl: "https://fylo-component-frontent-mentor.vercel.app/",
+    codeUrl: "https://github.com/Rem0ld/fylo-component-frontent-mentor",
+    imageUrl:
+      "https://raw.githubusercontent.com/Rem0ld/fylo-component-frontent-mentor/master/images/Screenshot_2020-11-06%20Frontend%20Mentor%20Fylo%20data%20storage%20component.png",
+    altImage: "Fylo component Frontend Mentor",
+  },
+  {
+    websiteUrl: "https://art-prev-frontend-mentor.vercel.app/",
+    codeUrl: "https://github.com/Rem0ld/art-prev-frontend-mentor",
+    imageUrl:
+      "https://raw.githubusercontent.com/Rem0ld/art-prev-frontend-mentor/master/images/Screenshot_2020-11-06%20Frontend%20Mentor%20Article%20preview%20component.png",
+    altImage: "Article preview Frontend Mentor",
+  },
+  {
+    websiteUrl: "https://faq-accor-frontend-mentor.vercel.app/",
+    codeUrl: "https://github.com/Rem0ld/faq-accor-frontend-mentor",
+    imageUrl:
+      "https://raw.githubusercontent.com/Rem0ld/faq-accor-frontend-mentor/master/images/Screenshot_2020-11-06%20Frontend%20Mentor%20FAQ%20Accordion%20Card.png",
+    altImage: "FAQ accordion Frontend Mentor",
+  },
+  {
+    websiteUrl:
+      "https://coding-bootcamp-frontendmentor-git-master.rem0ld.vercel.app/",
+    codeUrl: "https://github.com/Rem0ld/coding-bootcamp-frontendmentor",
+    imageUrl:
+      "https://raw.githubusercontent.com/Rem0ld/coding-bootcamp-frontendmentor/master/images/Screenshot_2020-11-06%20Frontend%20Mentor%20Coding%20Bootcamp%20Testimonials%20Slider.png",
+    altImage: "Testimonial Frontend Mentor",
+  },
+  {
+    websiteUrl: "https://media-dashboard-frontendmentor.vercel.app/",
+    codeUrl: "https://github.com/Rem0ld/media-dashboard-frontendmentor",
+    imageUrl:
+      "https://raw.githubusercontent.com/Rem0ld/media-dashboard-frontendmentor/master/images/Screenshot_2020-11-06%20Frontend%20Mentor%20%5BChallenge%20Name%20Here%5D.png",
+    altImage: "Social media dashboard Frontend Mentor",
+  },
+  {
+    websiteUrl: "https://job-listing-frontendmentor.vercel.app/",
+    codeUrl: "https://github.com/Rem0ld/job-listing-frontendmentor",
+    imageUrl:
+      "https://raw.githubusercontent.com/Rem0ld/job-listing-frontendmentor/master/Screenshot_2020-11-06%20React%20App.png",
+    altImage: "Job listing Frontend Mentor",
+  },
+  {
+    websiteUrl: "https://rest-api-country-frontend-mentor.vercel.app/",
+    codeUrl: "https://github.com/Rem0ld/rest-api-country-frontend-mentor",
+    imageUrl:
+      "https://raw.githubusercontent.com/Rem0ld/rest-api-country-frontend-mentor/master/Screenshot_2020-11-06%20Rest%20API%20countries.png",
+    altImage: "Rest API Country Frontend Mentor",
+  },
+];
