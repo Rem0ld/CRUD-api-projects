5673fcc2d4e6046cc04ec37e4a774c26f5a442ae
Rem0ld <p.lovergne@hotmail.fr>
2020-11-12 22:41:33 +0100

    add knex production connection

--- knexfile.js
+++ knexfile.js
@@ -9,4 +9,8 @@ module.exports = {
     client: "pg",
     connection: "postgres://localhost/test-projects",
   },
+  production: {
+    client: "pg",
+    connection: process.env.DATABASE_URL,
+  },
 };
