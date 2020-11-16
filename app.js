ff88aeb3609b1f1ccad4b40e6e4645a02f44272f
Rem0ld <p.lovergne@hotmail.fr>
2020-11-16 08:19:45 +0000

    cors open, preventing attack

--- app.js
+++ app.js
@@ -2,22 +2,41 @@ const createError = require("http-errors");
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const helmet = require("helmet");
 const cors = require("cors");
+const rateLimit = require("express-rate-limit");
 
 const app = express();
+
+app.set("trust proxy", 1);
+
+const limiter = rateLimit({
+  windowMs: 60 * 1000, // 1 minute
+  max: 100
+});
 
 const projects = require("./api/projects");
 
 app.use(logger("common"));
-app.use(express.json());
+app.use(express.json({limit: "10kb"}));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(helmet());
+app.use(limiter);
 
-app.use(
-  cors({
-    origin: "http://127.0.0.1:5500",
-  })
-);
+/*
+const whitelist = ['http://127.0.0.1:5500', 'https://rem0ld.github.io/'];
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+};
+*/
+app.use(cors());
 
 app.use("/api/v1/projects", projects);
 
